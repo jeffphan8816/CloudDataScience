48713a0c4133fd55c2292be51ae87863a26a4068
Jeff Phan <jeffphan8816@gmail.com>
2024-04-25 21:12:00 +1000

    indexing weather data in batch, a month at a time

--- fission/functions/weather.py
+++ fission/functions/weather.py
@@ -1,17 +1,23 @@
 from ftplib import FTP
 from elasticsearch import Elasticsearch
 import csv
-from elasticsearch.helpers import parallel_bulk
+from elasticsearch.helpers import bulk
+
+from io import BytesIO
 
 import warnings
 
 warnings.filterwarnings("ignore")
 
-url = 'https://elasticsearch:9200'
+url = 'https://localhost:9200/'
 user = "elastic"
 password = "cloudcomp"
 
 es = Elasticsearch([url], basic_auth=(user, password), verify_certs=False)
+
+if not es.ping():
+    raise ValueError("Connection failed")
+
 # Define the index name
 index_name = "weather_data"
 
@@ -34,33 +40,8 @@ mappings = {
 }
 
 # Check if the index exists
-if not es.indices.exists(index_name):
-    es.indices.create(index_name, body=mappings)
-# Function to print lines, skipping the first 13 lines
-line_count = 0
-
-def process_line(line):
-    global line_count
-    line_count += 1
-    if line_count > 13:
-        if line.startswith("Totals:"):
-            line_count = 0
-            return
-        row = line.split(",")
-        document = {
-            "Station Name": row[0],
-            "Date": row[1],
-            "Evapo-Rain": row[2],
-            "Rain": row[3],
-            "Pan-Rain": row[4],
-            "Max Temp": row[5],
-            "Min Temp": row[6],
-            "Max Humid": row[7],
-            "Min Humid": row[8],
-            "WindSpeed": row[9],
-            "UV": row[-1],
-        }
-        es.index(index="weather_data", document=document)
+if not es.indices.exists(index=index_name):
+    es.indices.create(index=index_name)
 
 
 # FTP server details
@@ -71,7 +52,7 @@ ftp_passwd = ""  # Your email address as the password
 # Connect to FTP server
 ftp = FTP(ftp_host)
 ftp.login(user=ftp_user, passwd=ftp_passwd)
-
+ftp.encoding = 'latin-1'
 # Start from the directory
 ftp.cwd("/anon/gen/clim_data/IDCKWCDEA0/tables/vic/")
 
@@ -80,20 +61,41 @@ surburbs = ftp.nlst()
 
 subdirectories = []
 # Loop through files
-for surburb in surburbs:
+for suburb in surburbs:
     # print(f"Content of {surburb}:" + "\n")
-    subdirectories.append(surburb)
+    subdirectories.append(suburb)
+
+bulk_data = []
 # Loop through subdirectories
 for subdirectory in subdirectories:
-    # fission function to call multiple threads
+    # Fission function to call multiple threads (not required for bulk API)
     files = ftp.nlst(subdirectory)
     for file in files:
+        data = []
         if file.endswith('.csv'):
-            # print(f"Content of {file}:")
-            temp = ftp.retrlines(f"RETR {file}", process_line)
-            # print(temp)
-            # with open(file, "rb") as file_obj:
-            #     ftp.retrbinary(f"RETR {file}", process_line())
-            line_count = 0
-# Close connection
+            # Process the file line by line and add data to bulk_data list
+            ftp.retrlines(f"RETR {file}", data.append)
+        data = data[13:-1]
+        bulk_data = []
+        for line in data:
+            row = line.split(",")
+            document = {
+                "_index": index_name,
+                "Station Name": row[0],
+                "Date": row[1],
+                "Evapo-Rain": row[2],
+                "Rain": row[3],
+                "Pan-Rain": row[4],
+                "Max Temp": row[5],
+                "Min Temp": row[6],
+                "Max Humid": row[7],
+                "Min Humid": row[8],
+                "WindSpeed": row[9],
+                "UV": row[-1],
+            }
+            bulk_data.append(document)
+
+        result = bulk(es, bulk_data, index=index_name)
+
+# Close FTP connection
 ftp.quit()
