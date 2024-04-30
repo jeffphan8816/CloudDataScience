b521dbcec5c7860243e60864add6eeb262f3b788
andreaunimelb <andrea.delahaye@student.unimelb.edu.au>
2024-04-30 17:58:40 +1000

    upload_crash created

---
+++ backend/data_functions/upload_crash.py
@@ -0,0 +1,24 @@
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+
+from crashdata_functions import get_crash_data
+
+
+
+url = 'https://elasticsearch:31001'
+user = "elastic"
+password = "cloudcomp"
+es = Elasticsearch([url], basic_auth=(user, password), verify_certs=False)
+
+if not es.ping():
+    raise ValueError("Connection failed")
+
+index_name = 'crashes'
+
+
+result_list = get_crash_data('../../data/sudo_tasmania_crash_2010_2020.csv')
+
+print(result_list)
+
+
+#bulk(es, c,index=)
