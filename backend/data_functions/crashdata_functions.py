de83738dd4cd7d99fcffed335c53fdde3faa77bc
andreaunimelb <andrea.delahaye@student.unimelb.edu.au>
2024-04-30 17:11:23 +1000

    filtering function for accepting new data from epa

--- backend/data_functions/crashdata_functions.py
+++ backend/data_functions/crashdata_functions.py
@@ -11,3 +11,17 @@ def clean_dataframe(file_path):
         df['severity'].apply(lambda severity : severity_dict[severity])
     except KeyError :
         pass           #TODO  add in logs
+
+def accepting_new_data(new_data, current_data):
+    #readings is a list of dictionary, each one is a reading of the air quality 
+
+    latest_current_df = current_data.groupby(['name', 'location'])['end'].max()
+
+    kept_data = new_data.copy()
+
+    for index in new_data.index:
+        if new_data.loc[index,'end'] <= latest_current_df.loc[new_data.loc[index,'name']
+                                                                          [new_data.loc[index,'location']],'end'] :
+            kept_data.drop(index, axis='index')
+    
+    return kept_data
