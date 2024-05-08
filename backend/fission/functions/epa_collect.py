249372da8aae839647290901ccee9606ca2e21f1
DillonBlake <dillonblake2020@gmail.com>
2024-05-08 11:03:19 +1000

    Added pagination

--- backend/fission/functions/epa_collect.py
+++ backend/fission/functions/epa_collect.py
@@ -8,9 +8,16 @@ from elasticsearch.helpers import bulk
 
 # Constant url of the epa
 URL = 'https://gateway.api.epa.vic.gov.au/environmentMonitoring/v1/sites/parameters?environmentalSegment=air'
+KEY = '96ff8ef9e03048e2bd2fa342a5d80587'
+
+ELASTIC_URL = 'https://elasticsearch:31001'
+ELASTIC_USER = "elastic"
+ELASTIC_PASSWORD = "cloudcomp"
+PULL_RATE = 100
 
 logger = logging.getLogger('epa_collect.py')
 logging.basicConfig(level=logging.INFO)
+
 
 def fetch_epa() -> list[dict]:
     """
@@ -20,7 +27,7 @@ def fetch_epa() -> list[dict]:
     """
     headers = {
         'Cache-Control': 'no-cache',
-        'X-API-Key': '96ff8ef9e03048e2bd2fa342a5d80587',
+        'X-API-Key': KEY,
         'User-agent': 'CloudCluster'
     }
     resp = requests.get(URL, headers=headers)
@@ -57,7 +64,7 @@ def fetch_epa() -> list[dict]:
                     cont = True
                     for key in ['since', 'until', 'averageValue']:
                         if key not in reading.keys():
-                             cont = False
+                            cont = False
                     if not cont:
                         continue
                     # Build dictionary and append
@@ -73,7 +80,7 @@ def fetch_epa() -> list[dict]:
     return out
 
 
-def accepting_new_data(new_data, current_data) -> list:
+def accepting_new_data(new_data: pd.DataFrame, current_data: pd.DataFrame) -> list[dict]:
     """
     Extract new data based on api return and current data
 
@@ -81,89 +88,107 @@ def accepting_new_data(new_data, current_data) -> list:
     @param current_data is the data in elastic search as a DataFrame
     @returns a list of what data needs to be inserted
     """
-    print(current_data['measure_name'].unique())
-    print(current_data['location'].unique())
-    latest_current_df = current_data.groupby(['measure_name', 'location'])['end'].max()
-    print(latest_current_df)
+    latest_current_df = current_data.groupby(
+        ['measure_name', 'location'])['end'].max()
     kept_data = new_data.copy()
 
     for index in new_data.index:
 
         name = new_data.loc[index, 'measure_name']
         # Need to convert coorinates to tuple
-        location = (new_data.loc[index,'location'][0] , new_data.loc[index,'location'][1])
+        location = (new_data.loc[index, 'location'][0],
+                    new_data.loc[index, 'location'][1])
 
         # Check for collisions
         if name in latest_current_df.index:
-            if location in latest_current_df[new_data.loc[index,'measure_name']].index :
-                if new_data.loc[index,'end'] <= latest_current_df[new_data.loc[index,'measure_name']][new_data.loc[index,'location']] :
-                    kept_data.drop(index, axis='index')
-    
+            if location in latest_current_df[new_data.loc[index, 'measure_name']].index:
+                if new_data.loc[index, 'end'] <= latest_current_df[new_data.loc[index, 'measure_name']][new_data.loc[index, 'location']]:
+                    kept_data = kept_data.drop(index, axis='index')
     return kept_data
 
-def upload(data, es):
+
+def upload(data: list[dict], es: Elasticsearch) -> None:
+    """
+    Upload the data to elastic search. If fail retry
+
+    @param data is a list of the data to insert
+    @param es is the elastic search instance
+    """
     cont = True
     while cont:
         try:
-            bulk(es, [data], index='airquality')
+            bulk(es, [data], index='airqualitytest')
             cont = False
             logger.info('Uploaded ' + str(data))
-        except : 
+        except:
             cont = True
             logger.warn('Failed to upload ' + str(data) + ' RETRYING')
 
 
-def main(): 
+def main():
     """
     Pull the most recent data from the EPA and check what needs to be inserted
     """
-    url = 'https://elasticsearch:31001'
-    user = "elastic"
-    password = "cloudcomp"
 
     # Connect to database
-    es = Elasticsearch([url], basic_auth=(user, password), verify_certs=False)
+    es = Elasticsearch([ELASTIC_URL], basic_auth=(ELASTIC_USER, ELASTIC_PASSWORD), verify_certs=False)
     if not es.ping():
         raise ValueError("Connection failed")
 
     # Fetch data
     new_data = fetch_epa()
-    df_new_data = pd.DataFrame.from_records(new_data,index=range(len(new_data))) 
-
-
-    # Get existing data after first date of new data
-    # oldest_start_new_data = df_new_data['start'].min()
-    query_res = es.search(index='airquality', body = {
-        "query": {
-            "match_all": {}
-        }
-    }) 
+    df_new_data = pd.DataFrame.from_records(
+        new_data, index=range(len(new_data)))
 
     # Switch coordinate order for new data and move to tuple
-    df_new_data['location'] = df_new_data['location'].apply(lambda location  : (location[1],location[0]))
+    df_new_data['location'] = df_new_data['location'].apply(
+        lambda location: (location[1], location[0]))
 
-    # Clean up the data returned from elastic search, convert list to tuple
-    query_list = [query_res['hits']['hits'][i]['_source'] for i in range(len(query_res['hits']['hits']))]
+    # Get existing data after first date of new data
+    oldest_start_new_data = df_new_data['start'].min()
+    # Will pull PULL_RATE at a time
+    query_list = []
+    cont = True
+    while cont:
+        query_res = es.search(index='airqualitytest',  body={
+            "query": {
+                "range": {
+                    "end": {
+                        "gte": oldest_start_new_data,
+                    }
+                }
+            },
+            "from": len(query_list),
+            "size": PULL_RATE})
+        # Clean up the data returned from elastic search, convert list to tuple
+        cleaned_list = [query_res['hits']['hits'][i]['_source']
+                        for i in range(len(query_res['hits']['hits']))]
+        if (len(cleaned_list) < PULL_RATE):
+            cont = False
+        query_list.extend(cleaned_list)
+
+    # Clean the pulled data
     if not len(query_list) == 0:
-        current_data = pd.DataFrame.from_records(query_list,index=range(len(query_list)))
-        print(current_data['measure_name'].unique())
-        current_data['location'] = current_data['location'].apply(lambda location  : (location[0],location[1]))
+        current_data = pd.DataFrame.from_records(
+            query_list, index=range(len(query_list)))
+        current_data['location'] = current_data['location'].apply(
+            lambda location: (location[0], location[1]))
         # Convert date strings
-        current_data['start'] = current_data['start'].apply(lambda s : datetime.strptime(s, '%Y-%m-%dT%H:%M:%S'))
-        current_data['end'] = current_data['end'].apply(lambda s : datetime.strptime(s, '%Y-%m-%dT%H:%M:%S'))
+        current_data['start'] = current_data['start'].apply(
+            lambda s: datetime.strptime(s, '%Y-%m-%dT%H:%M:%S'))
+        current_data['end'] = current_data['end'].apply(
+            lambda s: datetime.strptime(s, '%Y-%m-%dT%H:%M:%S'))
 
         # Remove collisions from new data
         to_upload = accepting_new_data(df_new_data, current_data)
     else:
         to_upload = df_new_data
 
-    print(to_upload)
-    print(to_upload['measure_name'].unique())
-    for line in to_upload.to_dict(orient='records') :
+    # Upload
+    for line in to_upload.to_dict(orient='records'):
         upload(line, es)
 
     return "Done"
 
-
-if __name__ == '__main__' :
+if __name__ == '__main__':
     main()
