caf08a362e9ab4eaa011e1ec8d55d5ad6987254e
DillonBlake <dillonblake2020@gmail.com>
2024-05-11 19:21:28 +1000

    Update data apis

--- backend/api/crime_api.py
+++ backend/api/crime_api.py
@@ -1,14 +1,12 @@
 from flask import request
 import json
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import scan
 
 ELASTIC_URL = 'https://172.26.135.52:9200'
 ELASTIC_USER = 'elastic'
 ELASTIC_PASSWORD = 'cloudcomp'
 ES_HEADERS = {'HOST': 'elasticsearch'}
-START_HEADER = 'X-Fission-Params-Start'
-SIZE_HEADER = 'X-Fission-Params-Size'
-BAD_PARAMS = json.dumps({'Status': 400, 'Message': 'Invalid Parameters'})
 NOT_FOUND = json.dumps({'Status': 500, 'Message': 'Internal Server Error'})
 
 es = Elasticsearch([ELASTIC_URL], basic_auth=(
@@ -16,23 +14,18 @@ es = Elasticsearch([ELASTIC_URL], basic_auth=(
 
 
 def main():
-    if START_HEADER not in request.headers or SIZE_HEADER not in request.headers:
-        return BAD_PARAMS
-
-    start = request.headers[START_HEADER]
-    size = request.headers[SIZE_HEADER]
-
     try:
-        results = es.search(index='crimes', body={
-            'from': start,
-            'size': size,
+        results = scan(es, index='crime', query={
             'query': {
                 'match_all': {}
             }
         })
-        as_list = [results['hits']['hits'][i]['_source']
-                   for i in range(len(results['hits']['hits']))]
-        out = {'status': 200, 'data': as_list}
+
+        data = []
+        for result in results:
+            data.append(result['_source'])
+
+        out = {'status': 200, 'data': data}
         return json.dumps(out)
     except:
         return NOT_FOUND
