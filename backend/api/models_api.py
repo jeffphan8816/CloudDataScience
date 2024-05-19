70d46f3725b505c5f4f6ee6c8b0d19985641b69d
andreaunimelb <andrea.delahaye@student.unimelb.edu.au>
2024-05-19 16:36:26 +1000

    update models api from local to fission and update makefile

--- backend/api/models_api.py
+++ backend/api/models_api.py
@@ -11,55 +11,17 @@ ELASTIC_USER = 'elastic'
 ELASTIC_PASSWORD = 'cloudcomp'
 ES_HEADERS = {'HOST': 'elasticsearch'}
 
-RESPONSE_HEADER = 'X-Fission-Params-Size'
+RESPONSE_HEADER = 'X-Fission-Params-Response'
 
 BAD_PARAMS = json.dumps({'Status': 400, 'Message': 'Invalid Parameters'})
 ERROR = json.dumps({'Status': 500, 'Message': 'Internal Server Error'})
 EMPTY = json.dumps({'Status': 200, 'Data': []})
 
-SCROLL = '5m'
 
 app = Flask(__name__)
 
-# Endpoint to handle query parameters
-@app.route('/api/models', methods=['GET'])
-def predict_from_pred():
-
-    with open('lin_reg_model_test.pkl', 'rb') as file:
-        loaded_model = pickle.load(file)
-
-    print(f'Loaded Model Type: {type(loaded_model)}')
-    print(f'Loaded Coefficients: {loaded_model.coef_}')
-    print(f'Loaded Intercept: {loaded_model.intercept_}')
-
-    # Read the a comma-separated list in the predictors argument
-    if 'predictors' in request.args:
-        predictors_str = request.args.get('predictors')
-        predictors = [float(value) for value in predictors_str.split(',')]
-
-        if len(predictors) != len(loaded_model.coef_):
-            return BAD_PARAMS
-
-        predictors = np.array(predictors).reshape(1,-1)
-
-        prediction = loaded_model.predict(predictors)[0]
-
-        print(f'Predictors: {predictors}')
-        print(f'Prediction {prediction}')
-
-        return {'prediction': prediction}
-
-    # Return the model as a dictionnary if no predictors have been specified
-    else:
-        model_dict = {'coef':list(loaded_model.coef_),
-                      'intercept':loaded_model.intercept_}
-        return model_dict
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
+# For local testing
+#@app.route('/api/models', methods=['GET'])
 def main():
     # Check parameters
     if RESPONSE_HEADER not in request.headers:
@@ -67,7 +29,41 @@ def main():
 
     # Get parameters
     try:
+        with open('lin_reg_model_test.pkl', 'rb') as file:
+            loaded_model = pickle.load(file)
 
-            return BAD_PARAMS
+        print(f'Loaded Model Type: {type(loaded_model)}')
+        print(f'Loaded Coefficients: {loaded_model.coef_}')
+        print(f'Loaded Intercept: {loaded_model.intercept_}')
+
+        # Read the a comma-separated list in the predictors argument
+        if 'predictors' in request.args:
+            predictors_str = request.args.get('predictors')
+            predictors = [float(value) for value in predictors_str.split(',')]
+
+            if len(predictors) != len(loaded_model.coef_):
+                return BAD_PARAMS
+
+            predictors = np.array(predictors).reshape(1,-1)
+
+            prediction = loaded_model.predict(predictors)[0]
+
+            print(f'Predictors: {predictors}')
+            print(f'Prediction {prediction}')
+
+            return {'prediction': prediction}
+
+        # Return the model as a dictionnary if no predictors have been specified
+        else:
+            model_dict = {'coef':list(loaded_model.coef_),
+                        'intercept':loaded_model.intercept_}
+            return model_dict
+
     except:
         return ERROR
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
