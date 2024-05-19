5a34516e3115251337f6d7031bb58617d38c1773
Alistair Wilcox <afwilcox@student.unimelb.edu.au>
2024-05-19 13:15:26 +0000

    Updates for working hourly weather

--- backend/fission/functions/deploy_weather.sh
+++ backend/fission/functions/deploy_weather.sh
@@ -39,22 +39,53 @@ fission fn update --name ingest-weather-obs --pkg ingest-weather-obs --entrypoin
 fission route delete --name ingest-weather-obs
 fission route create --method POST --url /ingest-weather-obs --function ingest-weather-obs --name ingest-weather-obs
 
+# Hourly weather obs ------------------------------------
+
+# fission package create --sourcearchive hourly-weather-obs.zip --env python-es --buildcmd "./build.sh" --name hourly-weather-obs
+# fission fn create --name hourly-weather-obs --pkg hourly-weather-obs --entrypoint "hourly-weather-obs.main"
+
+zip -jr hourly-weather-obs.zip hourly-weather-obs
+fission package update --sourcearchive hourly-weather-obs.zip --env python-es --buildcmd "./build.sh" --name hourly-weather-obs
+fission fn update --name hourly-weather-obs --pkg hourly-weather-obs --entrypoint "hourly-weather-obs.main"
+
+fission fn test --name hourly-weather-obs
+fission fn logs --name hourly-weather-obs
 
 
 
 # Todo: below
 
-
-
-
-fission timer create --name get-hourly-weather --function fetch-weather-obs --cron "15 * * * *"
+fission timer create --name get-hourly-weather --function hourly-weather-obs --cron "15 * * * *"
 fission timer delete --name get-hourly-weather
 
 
 
 
-# Testing
+# Testing ------------------------------------
 
+# Default
+curl -X GET http://172.26.135.52:9090/fetch-weather-obs -H "Host: fission"
+fission fn logs --name fetch-weather-obs
+
+# These should pass
+curl -X GET "http://172.26.135.52:9090/fetch-weather-obs?state=VIC&region=MALLEE" -H "Host: fission"
+fission fn logs --name fetch-weather-obs
+
+curl -X GET "http://172.26.135.52:9090/fetch-weather-obs?state=WA&region=KIMBERLEY" -H "Host: fission"
+fission fn logs --name fetch-weather-obs
+
+# Check with long region name with spaces
+curl -X GET 'http://172.26.135.52:9090/fetch-weather-obs?state=WA&region=CENTRAL%20WEST' -H "Host: fission"
+fission fn logs --name fetch-weather-obs
+
+# These should fail
+curl -X GET "http://172.26.135.52:9090/fetch-weather-obs?state=NSW&region=MALLEE" -H "Host: fission"
+curl -X GET "http://172.26.135.52:9090/fetch-weather-obs?state=NSW" -H "Host: fission"
+
+
+
+
+# Ingest
 fission fn test --name ingest-weather-obs
 fission fn logs --name ingest-weather-obs
 
@@ -75,53 +106,13 @@ fission fn logs --name ingest-weather-obs
 
 
 
-# Fetch weather obs ------------------------------------
-
-zip -jr fetch-weather-obs.zip fetch-weather-obs
-fission package update --sourcearchive fetch-weather-obs.zip --env python-multi --buildcmd "./build.sh" --name fetch-weather-obs
-fission fn update --name fetch-weather-obs --pkg fetch-weather-obs --entrypoint "fetch-weather-obs.main"
-
-fission fn test --name fetch-weather-obs
-fission fn logs --name fetch-weather-obs
-
-fission timer create --name get-hourly-weather --function fetch-weather-obs --cron "15 * * * *"
-fission timer delete --name get-hourly-weather
-
-
-
-# fission route create --method GET --url /fetch-weather-obs/{state}/{region} --function fetch-weather-obs --name fetch-weather-obs
 
 
 
 
-curl -X GET http://172.26.135.52:9090/fetch-weather-obs/VIC/MALLEE \
-     -H "Host: fission"
-
-curl -X GET http://172.26.135.52:9090/fetch-weather-obs/NSW/MALLEE \
-     -H "Host: fission"
-
-curl -X GET http://172.26.135.52:9090/fetch-weather-obs/NSW \
-     -H "Host: fission"
-
-# Default
-curl -X GET http://172.26.135.52:9090/fetch-weather-obs -H "Host: fission"
-fission fn logs --name fetch-weather-obs
-
-# These should pass
-curl -X GET "http://172.26.135.52:9090/fetch-weather-obs?state=VIC&region=MALLEE" -H "Host: fission"
-fission fn logs --name fetch-weather-obs
-
-curl -X GET "http://172.26.135.52:9090/fetch-weather-obs?state=WA&region=KIMBERLEY" -H "Host: fission"
-fission fn logs --name fetch-weather-obs
-
-# Check with long region name with spaces
-curl -X GET 'http://172.26.135.52:9090/fetch-weather-obs?state=WA&region=CENTRAL%20WEST' -H "Host: fission"
-fission fn logs --name fetch-weather-obs
 
 
-# These should fail
-curl -X GET "http://172.26.135.52:9090/fetch-weather-obs?state=NSW&region=MALLEE" -H "Host: fission"
-curl -X GET "http://172.26.135.52:9090/fetch-weather-obs?state=NSW" -H "Host: fission"
+
 
 
 
