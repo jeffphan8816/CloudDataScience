b79213c82972b3b5f99ed2adac0d553ba075d31d
PaoPaoYue <846260131@qq.com>
2024-05-22 13:40:45 +0000

    Small fixes

--- docs/content/doc_api.py
+++ docs/content/doc_api.py
@@ -1,10 +1,12 @@
 from flask import request
+import os
 
 def main():
     FILE_HEADER = 'X-Fission-Params-File'
+    current_dir = os.path.dirname(__file__)
     try:
         file_name = request.headers[FILE_HEADER]
-        with open(file_name, 'rt') as file:
+        with open(os.path.join(current_dir, file_name), 'rt') as file:
             return file.read()
     except:
         return '404'
